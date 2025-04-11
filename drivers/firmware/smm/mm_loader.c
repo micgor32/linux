51478beb7c24a8706dfa171c6e111c60f9179bf4
Michal Gorlas <michal.gorlas@9elements.com>
2025-04-11 22:49:16 +0200

    fix: trigger code
    The trigger code correctly retrieves data from SMI handler,
    post-boot locking and unlocking is functional, although,
    synchronization still remains WIP.
    
    Signed-off-by: Michal Gorlas <michal.gorlas@9elements.com>

--- drivers/firmware/smm/mm_loader.c
+++ drivers/firmware/smm/mm_loader.c
@@ -46,15 +46,17 @@ static struct smm_data get_cb_data(void)
 
 static int trigger_smi(uint64_t cmd, uint64_t arg, uint64_t retry){
 	// the values here have to be 64bit otherwise the compiler will cmplain
-	uint64_t status = 0;
-	asm (
+	uint64_t status;
+	uint16_t apmc_port = 0xb2;
+	asm volatile (
 		"movq	%[cmd],  %%rax\n\t"
 		"movq   %%rax,	%%rcx\n\t"
 		"movq	%[arg],  %%rbx\n\t"
 		"movq	%%rbx,	 %%rdx\n\t"
 		"movq   %[retry],  %%r8\n\t"
 		".trigger:	  \n\t"
-		"out	%%al, $0xb2\n\t"
+		"mov	%[apmc_port], %%dx\n\t"
+		"outb	%%al, %%dx\n\t"
 		"cmpq	%%rcx, %%rax\n\t"
 		"jne .return_changed\n\t"
 		"pushq  %%rcx\n\t"
@@ -68,21 +70,24 @@ static int trigger_smi(uint64_t cmd, uint64_t arg, uint64_t retry){
 		".return:\n\t" // do nothing here otherwise we get an compiler warning
 		"jmp	.return_not_changed\n\t"
 		".return_changed:\n\t"
+		"movw $0x3f8, %%dx\n\t"
+		"movb $'3', %%al\n\t"
+	        "outb %%al, %%dx\n\t"
 		"movq	%%rax, %[status]\n\t"
+		"jmp	.end\n\t"
 		".return_not_changed:"
+		"movw $0x3f8, %%dx\n\t"
+		"movb $'n', %%al\n\t"
+		"outb %%al, %%dx\n\t"
 		"movq	%%rcx, %[status]\n\t"
+		".end:\n\t"
+		// this is getting out of control, these jumps are mostly unnecessary
 		: [status] "=r" (status)
-		: [cmd] "r" (cmd), [arg] "r" (arg), [retry] "r" (retry)
-		: "%rax", "%rbx", "%rdx", "%rcx", "%r8", "%al"
+		: [cmd] "r" (cmd), [arg] "r" (arg), [retry] "r" (retry) , [apmc_port] "r" (apmc_port)
+		: "%rax", "%rbx", "%rdx", "%rcx", "%r8", "%al" , "%dx"
 	);
-	/*uint32_t test = 0;*/
-	/*asm volatile (*/
-	/*	"movq %[cmd], %%rax\n\t"*/
-	/*	"movq %%rax, %%rcx\n\t"*/
-	/*	: //[test] "=rm" (test)*/
-	/*	: [cmd] "rm" (cmd)*/
-	/*	: "%rax");*/
-	printk(KERN_INFO "SMI returned 0x%llx", status);
+
+	printk(KERN_INFO "SMI returned %llx", ((status >> 8) & 0xff));
 	
 	if(status == cmd) {
 		printk(KERN_INFO "rax not modified by smm");
@@ -100,6 +105,7 @@ static int unlock_smram(struct smm_data *data)
 
 	cmd = data->mm_info.register_mm_entry_swsmi | (PAYLOAD_MM_UNLOCK_SMRAM << 8);
 	printk(KERN_INFO "cmd is 0x%llx\n", cmd);
+	printk(KERN_INFO "so it will be shifted to %x", ((cmd >> 8) & 0xff));
 	status = trigger_smi(cmd, 0, 5); // 5 here (and below) just because, why not repeat smi issuing 5 times :D 
 	return status;
 }
@@ -134,30 +140,30 @@ static void notrace test(void *unused)
 static int __init mm_loader_init(void)
 {
 	struct smm_data cb_data = get_cb_data();
-	u32 entry_point;
+	uint32_t entry_point;
 
-	/*if (unlock_smram(&cb_data))*/
-	/*	return -1;*/
+	if (unlock_smram(&cb_data))
+		return -1;
 
 	// so now we can install the entry point
 	
 	// first see whether this is even correct
-	if (!cb_data.mm_info.requires_long_mode_call)
-		entry_point = __pa(real_mode_header->startup_64);
-	else
-		entry_point = __pa(real_mode_header->startup_32);
-
-	printk(KERN_INFO "pa of entry point 0x%x\n", entry_point);
-	is_for_smm = SMM_INIT_HANDLER;
-	ending_code = (unsigned long)test; // for now its just this dummy function, next step would be to place the functions needed here in smram
+	/*if (!cb_data.mm_info.requires_long_mode_call)*/
+	/*	entry_point = __pa(real_mode_header->startup_64);*/
+	/*else*/
+	/*	entry_point = __pa(real_mode_header->startup_32);*/
+	/**/
+	/*printk(KERN_INFO "pa of entry point 0x%lx\n", entry_point);*/
+	/*is_for_smm = SMM_INIT_HANDLER;*/
+	/*ending_code = (unsigned long)test; // for now its just this dummy function, next step would be to place the functions needed here in smram*/
 	
 	// here comes smi registering entry point
-	if (register_entry_point(&cb_data, entry_point))
-		return -1;
+	/*if (register_entry_point(&cb_data, entry_point))*/
+	/*	return -1;*/
 
 	/*// assuming the handler is loaded \land the entry point is registered:*/
-	/*if (lock_smram(&cb_data))*/
-	/*	return -1;*/
+	if (lock_smram(&cb_data))
+		return -1;
 
 	return 0;
 }
